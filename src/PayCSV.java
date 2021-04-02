d7e57ff396e981ef8e57ec6fbbfe02e98a635212
max.segale <max.segale@fcbcure.com>
2021-04-01 22:39:35 -0400

    New comments

--- src/PayCSV.java
+++ src/PayCSV.java
@@ -11,24 +11,30 @@ public class PayCSV {
 
     CSVWriter csvWriter = new CSVWriter(new FileWriter("/Users/maxwell/Documents/paystubs/pay-days.csv"));
 
+    // Write column names
     csvWriter.writeNext(new String[]{"Date", "Amount", "Total"});
 
+    // Set data formats
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    NumberFormat formatter = NumberFormat.getCurrencyInstance();
+    NumberFormat dollarFormat = NumberFormat.getCurrencyInstance();
 
+    // Set initial dollar amount 
     Double totalPay = 0.0;
 
+    // Loop through pay days
     for (int w = 0; w < payDayList.size(); w++) {
 
       PayDay payItem = payDayList.get(w);
 
+      // Add to total dollar amount
       totalPay += payItem.amount;
 
+      // Format data to strings
       String weekDate = dateFormat.format(payItem.date);
-      String dollarAmount = formatter.format(payItem.amount);
-      String totalAmount = formatter.format(totalPay);
+      String dollarAmount = dollarFormat.format(payItem.amount);
+      String totalAmount = dollarFormat.format(totalPay);
 
-      // Write data line
+      // Write CSV line
       csvWriter.writeNext(new String[]{weekDate, dollarAmount, totalAmount});
     }
 
